0e4d3536cf1d2a9985f6983b4c4075e8caef386b
unknown <montinformatic233@gmail.com>
2023-04-07 20:03:45 -0500

    list of added products

--- src/components/Paginas/Inicio.jsx
+++ src/components/Paginas/Inicio.jsx
@@ -1,3 +1,4 @@
+import {ropas} from '../../data/ropa'
 import './Inicio.css'
 export function Inicio(){
     return (
@@ -8,8 +9,23 @@ export function Inicio(){
             <div className="section__results">
                 <span>Viendo 1-10 de 50 resultados</span>
             </div>
-            <div className="section__productos">
-                
+            <div className="section__products">
+                {
+                    ropas.map((ropa)=>(
+                       <div className='product'>
+                            <div className="product__content_img">
+                                <img src={ropa.imagen} alt="" className='product__img' width={200}/>
+                            </div>
+                            <div className="product__details">
+                                <span className='product__name'>{ropa.nombre}</span>
+                                <span className='product__price'>{ropa.precio}</span>
+                                <button className='product__add'>
+                                    Agregar al Carrito
+                                </button>
+                            </div>
+                       </div> 
+                    ))
+                }
             </div>
             <div className="pages">
                 <div className="pages__number pages__number--check">1</div>
---
+++ src/data/ropa.js
@@ -0,0 +1,62 @@
+export const ropas=[
+    {
+        'id_ropa': 1,
+        'nombre': 'Vaqueros RUMAA HEMP true mid blue',
+        'imagen': 'https://www.veganized.es/16847-thickbox_default/vaqueros-rumaa-hemp-true-mid-blue.jpg',
+        'precio': 60
+    },
+    {
+        'id_ropa': 2,
+        'nombre': 'Blusa FREYAA',
+        'imagen':'https://www.veganized.es/12837-thickbox_default/blusa-freyaa.jpg',
+        'precio': 30
+    },
+    {
+        'id_ropa': 3,
+        'nombre': 'Blusa MEMAA SKETCH OF',
+        'imagen': 'https://www.veganized.es/16698-thickbox_default/blusa-memaa-sketch-of.jpg',
+        'precio': 35
+    },
+    {
+        'id_ropa': 4,
+        'nombre': 'Camisa QUAASI cloudbusting',
+        'imagen': 'https://www.veganized.es/15841-large_default/camisa-quaasi-cloudbusting.jpg',
+        'precio': 40
+    },
+    {
+        'id_ropa': 5,
+        'nombre': 'Cardigan AAGADI COMPACT acid black',
+        'imagen': 'https://www.veganized.es/14262-large_default/cardigan-aagadi-compact-acid-black.jpg',
+        'precio': 50
+    },
+    {
+        'id_ropa': 6,
+        'nombre': 'Pijama RETRO LONG JOHN palmeras',
+        'imagen': 'https://www.veganized.es/16059-thickbox_default/pijama-retro-long-john.jpg',
+        'precio': 80
+    },
+    {
+        'id_ropa': 7,
+        'nombre': 'Pijama RETRO LONG JOHN lemon',
+        'imagen': 'https://www.veganized.es/16061-thickbox_default/pijama-retro-long-john.jpg',
+        'precio': 80
+    },
+    {
+        'id_ropa': 8,
+        'nombre': 'LONG JOHN RETRO Terry Pijama Agua stripe',
+        'imagen': 'https://www.veganized.es/14525-thickbox_default/long-john-retro-terry-pijama-agua-stripe.jpg',
+        'precio': 75
+    },
+    {
+        'id_ropa': 9,
+        'nombre': 'Tank Top Basket verde',
+        'imagen': 'https://www.veganized.es/11287-thickbox_default/tank-top-basket-verde.jpg',
+        'precio': 50
+    },
+    {
+        'id_ropa': 10,
+        'nombre': 'Nilo - Sneaker negro',
+        'imagen': 'https://www.veganized.es/11050-thickbox_default/nilo-sneaker-negro.jpg',
+        'precio': 80
+    },
+]
