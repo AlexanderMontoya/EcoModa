a33be36f8c57537725e29022d3bf94b8d2b56729
unknown <montinformatic233@gmail.com>
2023-07-03 19:36:43 -0500

    new routes for the images

--- src/data/ropa.js
+++ src/data/ropa.js
@@ -2,61 +2,61 @@ export const ropas=[
     {
         'id_ropa': 1,
         'nombre': 'Vaqueros RUMAA HEMP true mid blue',
-        'imagen': 'https://www.veganized.es/16847-thickbox_default/vaqueros-rumaa-hemp-true-mid-blue.jpg',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/16847-Vaqueros-RUMAA-HEMP-true-mid-blue.jpg',
         'precio': 60
     },
     {
         'id_ropa': 2,
         'nombre': 'Blusa FREYAA',
-        'imagen':'https://www.veganized.es/12837-thickbox_default/blusa-freyaa.jpg',
+        'imagen':'https://www.veganized.es/wp-content/uploads/12837-Blusa-FREYAA.jpg',
         'precio': 30
     },
     {
         'id_ropa': 3,
         'nombre': 'Blusa MEMAA SKETCH OF',
-        'imagen': 'https://www.veganized.es/16698-thickbox_default/blusa-memaa-sketch-of.jpg',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/16698-Blusa-MEMAA-SKETCH-OF-1067x1536.jpg',
         'precio': 35
     },
     {
         'id_ropa': 4,
-        'nombre': 'Camisa QUAASI cloudbusting',
-        'imagen': 'https://www.veganized.es/15841-large_default/camisa-quaasi-cloudbusting.jpg',
+        'nombre': 'Camisa Lumberjacks Black',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/10353-Camisa-Lumberjacks-Black.jpg',
         'precio': 40
     },
     {
         'id_ropa': 5,
-        'nombre': 'Cardigan AAGADI COMPACT acid black',
-        'imagen': 'https://www.veganized.es/14262-large_default/cardigan-aagadi-compact-acid-black.jpg',
+        'nombre': 'Cardigan infantil de punto HURIT',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/12501-Cardigan-infantil-de-punto-HURIT.jpg',
         'precio': 50
     },
     {
         'id_ropa': 6,
         'nombre': 'Pijama RETRO LONG JOHN palmeras',
-        'imagen': 'https://www.veganized.es/16059-thickbox_default/pijama-retro-long-john.jpg',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/16059-Pijama-RETRO-LONG-JOHN-palmeras.jpg',
         'precio': 80
     },
     {
         'id_ropa': 7,
-        'nombre': 'Pijama RETRO LONG JOHN lemon',
-        'imagen': 'https://www.veganized.es/16061-thickbox_default/pijama-retro-long-john.jpg',
+        'nombre': 'Pijama LONG JOHN RETRO Bears',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/9711-Pijama-LONG-JOHN-RETRO-Bears.jpg',
         'precio': 80
     },
     {
         'id_ropa': 8,
         'nombre': 'LONG JOHN RETRO Terry Pijama Agua stripe',
-        'imagen': 'https://www.veganized.es/14525-thickbox_default/long-john-retro-terry-pijama-agua-stripe.jpg',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/14522-LONG-JOHN-RETRO-Terry-Pijama-Agua-stripe.jpg',
         'precio': 75
     },
     {
         'id_ropa': 9,
         'nombre': 'Tank Top Basket verde',
-        'imagen': 'https://www.veganized.es/11287-thickbox_default/tank-top-basket-verde.jpg',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/11287-Tank-Top-Basket-verde.jpg',
         'precio': 50
     },
     {
         'id_ropa': 10,
-        'nombre': 'Nilo - Sneaker negro',
-        'imagen': 'https://www.veganized.es/11050-thickbox_default/nilo-sneaker-negro.jpg',
+        'nombre': 'FairTrainer WHITE CAP HIGH azul/blanco',
+        'imagen': 'https://www.veganized.es/wp-content/uploads/4591-Fair-Trainer-White-Cap-Hi-Cut-Classic-Ocean-Blue-Just-White.jpg',
         'precio': 80
     },
 ]
